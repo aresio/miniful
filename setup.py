502227f3821efcea2caf088edb8052d464725550
Marco S. Nobile <nobile@disco.unimib.it>
2018-03-28 00:37:50 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 from distutils.core import setup
 setup(
-  name = 'minuful',
+  name = 'miniful',
   packages = ['miniful'], # this must be the same as the name above
-  version = '0.0.1',
+  version = '0.0.4',
   description = 'Minimal Fuzzy Library',
   author = 'Marco S. Nobile',
   author_email = 'nobile@disco.unimib.it',
   url = 'https://github.com/aresio/stfu', # use the URL to the github repo
-  #download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['fuzzy logic', 'sugeno', 'reasoner'], # arbitrary keywords
   license='LICENSE.txt',
-  long_description=open('README.md').read(),
   install_requires=[
         "numpy >= 1.12.0",
         "scipy >= 1.0.0"
     ],
-  #include_package_data=True,
   classifiers = ['Programming Language :: Python :: 2.7'],
- # package_data={'fstpso': ['pso_1st_half_2.fcl', 'pso_2nd_half_2.fcl'], },
 )
